77011df05c3637340acc9830cf073e1be579d8b1
Nils Langner <contact@phmlabs.com>
2016-04-23 10:07:31 +0200

    path to say command can be set

--- Synthesizer/Say.php
+++ Synthesizer/Say.php
@@ -4,9 +4,18 @@ namespace phmLabs\ConsoleSpeechDecorator;
 
 class Say implements Synthesizer
 {
+    private $sayCommand = 'say';
+
+    public function __construct($pathToSayCommand = null)
+    {
+        if ($pathToSayCommand) {
+            $this->sayCommand = $pathToSayCommand;
+        }
+    }
+
     public function textToSpeech($message)
     {
-        $command = 'say "' . strip_tags($message) . '"';
+        $command = $this->sayCommand . ' "' . strip_tags($message) . '"';
         exec($command);
     }
 }
